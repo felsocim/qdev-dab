a546cb21e45901770d3c2489a86518d4066c2d96
Marek Felšöci <marek@felsoci.sk>
2026-03-26 11:14:34 +0100

    Test refusal (first case)

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,6 +5,9 @@ class checkWithdrawalUnitTests(unittest.TestCase):
   def test_authorized(self):
     result = checkWithdrawal(80, 576)
     self.assertTrue(result)
+  def test_refusal(self):
+    result = checkWithdrawal(160, 90)
+    self.assertFalse(result)
 
 if __name__ == '__main__':
   unittest.main()
